be0fefb07637d843590da607a1313daac21084de
Ranis <khaertdinovranis@gmail.com>
2023-05-28 17:34:43 +0300

    work with svetleKit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<nav>
+	<div class="nav-container">
+		<a href="/" class="nav-logo" title="Back to Homepage">Ranis Haertdinov</a>
+		<div class="nav-links">
+			<a href="/about" class="link">About me</a>
+			<a href="/blog" class="link">Blog</a>
+			<a href="/projects" class="link">My projects</a>
+			<a href="/contact" class="link">Contacts</a>
+		</div>
+	</div>
+</nav>
+
+<div class="container">
+	<slot></slot>
+</div>
+
+
+<style>
+	.container {
+		max-width: 1400px;
+		margin: 50px auto;
+	}
+</style>
